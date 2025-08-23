44307334578125d9f72e0c3541a0db1dd68420d1
Kabir Hossain <kabir.hossain@nitex.info>
2025-08-23 13:09:02 +0600

    chore: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import unusedImports from 'eslint-plugin-unused-imports';
+import importPlugin from 'eslint-plugin-import';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite
+    ],
+    languageOptions: {
+      ecmaVersion: 2023,
+      globals: globals.browser,
+      sourceType: 'module'
+    },
+    plugins: {
+      'unused-imports': unusedImports,
+      import: importPlugin
+    },
+    rules: {
+      // Remove unused imports & variables automatically
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_'
+        }
+      ],
+
+      // Sort imports
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          alphabetize: { order: 'asc', caseInsensitive: true }
+        }
+      ]
+    }
+  },
+  ...eslintConfigPrettier
+]);
